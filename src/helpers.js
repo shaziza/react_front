836227c77ca9e557b393b9533422e4fdb6f04630
aziza <shakirova-aziza@mail.ru>
2020-12-11 15:31:24 +0800

    fix

--- src/helpers.js
+++ src/helpers.js
@@ -1,24 +1,47 @@
 // @ts-check
 /**
- * 
- *
- * @param {object} obj 
- * @param {string} path 
- */
+ * Return field by given string path
+ * @param {any} obj
+ * @param {any} path
+ * @return {any}
+*/
 export const getPathFromObjectByArray = (obj, path) => {
 	let key = path.pop();
 	let result = path.length !== 0 ? getPathFromObjectByArray(obj, path) : obj;
 	return result ? result[key] : result;
 }
+/**
+ * Return field by given string path
+ * @param {Object} obj
+ * @param {String} path
+ * @param {any} defaultValue
+ * @return {any}
+*/
 export const getPathFromObject = (obj, path, defaultValue) => {
 	let keys = path.split('.');
 	let result = getPathFromObjectByArray(obj, keys);
 	return result === undefined ? defaultValue : result;
 }
+/**
+ * @param {Object} obj
+ * @param {any} path
+ * @param {string} value
+ * @param {any} action
+ * @param {any} options
+ * @return {any}
+*/
 export const assignToPathFromObjectToValue = (obj, path, value, action, options) => {
 	let keys = path.split('.');
 	return findPathAndAssign(obj, keys, value, action, options);
 }
+/**
+ * @param {any} obj
+ * @param {any} keys
+ * @param {string} value
+ * @param {any} action
+ * @param {any} options
+ * @return {any}
+*/
 export const findPathAndAssign = (obj, keys, value, action, options) => {
 	if (keys.length === 0) {
 		return action ? action(obj, value, options) : value;
@@ -27,11 +50,23 @@ export const findPathAndAssign = (obj, keys, value, action, options) => {
 	let curr = Array.isArray(obj[key]) ? [...obj[key]] : Object.assign({}, obj[key]);
 	return Object.assign(obj, {[key]: findPathAndAssign(curr, keys, value, action, options)});
 }
+/**
+ * @param {any} arr
+ * @param {any} value
+ * @param {any} deep_level
+ * @return {any}
+*/
 export const appendInArray = (arr, value, {deep_level = 0} = {}) => {
 	if (Array.isArray(value)) return [...arr, ...value];
 	if (typeof(value) === 'object') return appendInObject(arr, value, {deep_level});
 	return Array.isArray(arr) ? [...arr, value] : (value);
-};
+}
+/**
+ * @param {any} obj
+ * @param {any} value
+ * @param {any} deep_level
+ * @return {any}
+*/
 export const appendInObject = (obj, value, {deep_level} = {}) => {
 	if (deep_level > 0) {
 		let result = Object.assign({}, obj);
@@ -49,6 +84,7 @@ export const appendInObject = (obj, value, {deep_level} = {}) => {
 		return obj ? Object.assign({}, obj, value) : value;
 	}
 }
+// @ts-ignore
 export function BEMName(block = '', element = '', modificators = [], extra) {
 	const name = block + (element ? ('__' + element) : '');
 	return name +
